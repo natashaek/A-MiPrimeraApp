eecc381ba2f58ca8c3b00753e05df013e95f2fda
nattashaek <natasha.ek@cbtis72.edu.mx>
2022-02-19 23:04:35 -0500

    Saludos desarrollador

--- app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -2,12 +2,38 @@ package com.example.miprimeraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bSaludar = findViewById<Button>(R.id.bSaludar)
+        val etNombre = findViewById<EditText>(R.id.etNombre)
+        val cbDev = findViewById<CheckBox>(R.id.cbDeveloper)
+
+        bSaludar.setOnClickListener(View.OnClickListener {
+            if (validaDat()) {
+                if (cbDev.isChecked) {
+                    Toast.makeText(
+                        this,
+                        "Bienvenido, " + etNombre.text + ", sé que eres un desarrollador.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(this, "Bienvenido, " + etNombre.text, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                    Toast.makeText(this, "Escribe tu nombre para saludarte :/ ", Toast.LENGTH_SHORT).show()
+                }
+
+
+        })
     }
 
     fun validaDat():Boolean {
