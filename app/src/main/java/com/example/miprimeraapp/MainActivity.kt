71266abe18a4d584d260778ea15a8ddf159e5fd8
nattashaek <natasha.ek@cbtis72.edu.mx>
2022-02-19 22:37:20 -0500

    Uniendo vista con codigo

--- app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.miprimeraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun validaDat():Boolean {
+        val etNombre = findViewById<EditText>(
+         R.id.etNombre
+        )
+        val nombreUsuario = etNombre.text
+        if (nombreUsuario.isNullOrEmpty()) {
+            return false;
+        }
+
+        return true;
     }
 }
